0f90ead5e3743d49f6f730f7de6e0faf5b64b687
Max <max@Maxs-MacBook-Air.local>
2025-08-16 19:56:07 +0200

    update url import to esm

--- src/lib/search/SearchAPI.ts
+++ src/lib/search/SearchAPI.ts
@@ -1,4 +1,4 @@
-import { URL } from 'https://deno.land/std@0.208.0/node/url.ts';
+import { URL } from 'https://esm.sh/url';
 import { type ImageFormat } from '../types/Image.ts';
 import { URLS } from '../utils/Constants.ts';
 import SearchResultsParser from './SearchResultsParser.ts';
